66f2795db0748e74eb9122a6d937db9eaece048c
Anton Sementsov <anton.sementsov@unymira.com>
2018-12-20 21:13:35 +0100

    added a logout function

--- src/include/templating/navigation.php
+++ src/include/templating/navigation.php
@@ -1,3 +1,6 @@
+<?php
+?>
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
   <a class="navbar-brand" href="#">PM Tool</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,9 +9,9 @@
   <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="#">Projects <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Calender</a>
-      <a class="nav-item nav-link" href="#">About</a>
-      <form class="form-inline rightButton">
+      <a class="nav-item nav-link" href="calender.php">Calender</a>
+      <a class="nav-item nav-link" href="about.php">About</a>
+      <form class="form-inline rightButton" action="../module/logout.php">
      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
    </form>
     </div>
